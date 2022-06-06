28872aaa5a9740a492730bfe5fea14183cb9b7d3
cyvaer <cyvaer@gmail.com>
2022-06-06 05:25:41 -0500

    Add getDestinationPivotTable, UpdateFlightPivotTable functions

--- app.js
+++ app.js
@@ -1,7 +1,31 @@
-
+const flights = require('./flight_records');
 
 var flightsDestinationsPivotTable =
     {
         'AA': { 'BNA': 'None', 'DEN': 'None', 'DFW': 'None', 'IAD': 'None', 'MIA': 'None', 'ORD': 'None', 'STL':'None' },
         'UA': { 'BNA': 'None', 'DEN': 'None', 'DFW': 'None', 'IAD': 'None', 'MIA': 'None', 'ORD': 'None', 'STL':'None' }
     };
+
+function updateFlightPivotTable(flight){
+
+    const destinationAccumulator =  flightsDestinationsPivotTable[flight.Carrier][flight.Destination];
+
+    // If flight accumulator is equal to the string 'None',
+    // then change it to number equal to zero to be able to sum + 1
+    if( typeof destinationAccumulator === 'string' ){
+        flightsDestinationsPivotTable[flight.Carrier][flight.Destination] = 0;
+    }
+    flightsDestinationsPivotTable[flight.Carrier][flight.Destination]++;
+
+}
+
+function getDestinationsPivotTable(){
+
+    flights.forEach(flight => {
+        updateFlightPivotTable(flight);
+    });
+    return flightsDestinationsPivotTable;
+
+}
+
+console.log(getDestinationsPivotTable());

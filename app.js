443122528692c298aa28c98eb274b17e3c1f8dde
cyvaer <cyvaer@gmail.com>
2022-06-06 05:07:14 -0500

    Add specific (no generic solution) flightDestinationTable object literal

---
+++ app.js
@@ -0,0 +1,7 @@
+
+
+var flightsDestinationsPivotTable =
+    {
+        'AA': { 'BNA': 'None', 'DEN': 'None', 'DFW': 'None', 'IAD': 'None', 'MIA': 'None', 'ORD': 'None', 'STL':'None' },
+        'UA': { 'BNA': 'None', 'DEN': 'None', 'DFW': 'None', 'IAD': 'None', 'MIA': 'None', 'ORD': 'None', 'STL':'None' }
+    };
